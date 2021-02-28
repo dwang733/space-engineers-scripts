7ad062f4ab91165b84218a0f80fd07845295b9e6
David Wang <dwang733@outlook.com>
2021-02-28 00:32:29 -0800

    Half-finished pull ingots from refineries

--- Scripts/Inventory Management/Pyro's Inventory Manager/Program.cs
+++ Scripts/Inventory Management/Pyro's Inventory Manager/Program.cs
@@ -22,62 +22,80 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
+        /// <summary>
+        /// The grid terminal system replacement.
+        /// </summary>
+        private new readonly EnhancedGTS GridTerminalSystem;
+
+        /// <summary>
+        /// The user-customizable settings.
+        /// </summary>
+        private readonly Settings _settings;
+
+        /// <summary>
+        /// The list of ingot containers.
+        /// </summary>
+        private readonly List<IMyCargoContainer> _ingotContainers = new List<IMyCargoContainer>();
+
+        private readonly List<IMyRefinery> _refineries = new List<IMyRefinery>();
 
         public Program()
         {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set Runtime.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
-        }
+            GridTerminalSystem = new EnhancedGTS(this);
+            _settings = new Settings(Me.CustomData);
 
-        public void Save()
-        {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
+            //GridTerminalSystem.GetBlocksOfType(_ingotContainers, container => _settings.IngotContainerKeywords.Any(keyword => container.CustomName.Contains(keyword)));
+            //GridTerminalSystem.GetBlocksOfType(_refineries, refinery => _settings.RefineryKeywords.Contains(refinery.CustomName));
+
+            //Echo($"{_ingotContainers.Count()} | {_refineries.Count()}");
+
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from. Be aware that the
-            // updateSource is a  bitfield  and might contain more than 
-            // one update type.
-            // 
-            // The method itself is required, but the arguments above
-            // can be removed if not needed.
+            try
+            {
+                //var ingotContainersEnumerator = _ingotContainers.GetEnumerator();
+                //var refineryOutput = new List<MyInventoryItem>();
+                //// Transfer output inventory of each refinery to ingot containers.
+                //// We assume that if an ingot container cannot be transferred to, it will remain so for the rest of this loop.
+                //foreach (var refinery in _refineries)
+                //{
+                //    var refineryInventory = refinery.OutputInventory;
+                //    refineryInventory.GetItems(refineryOutput);
+
+                //    // Try to transfer each item in refinery output to an ingot container.
+                //    foreach (var item in refineryOutput)
+                //    {
+                //        var success = refineryInventory.TransferItemTo(ingotContainersEnumerator.Current.GetInventory(), item);
+                //        while (!success)
+                //        {
+                //            ingotContainersEnumerator.MoveNext();
+                //            success = refineryInventory.TransferItemTo(ingotContainersEnumerator.Current.GetInventory(), item);
+                //        }
+                //    }
+                //}
+
+                Echo($"{Runtime.LastRunTimeMs}");
+                //GridTerminalSystem.GetBlocksOfType(_refineries, refinery => _settings.RefineryKeywords.Any(keyword => refinery.CustomName.Contains(keyword)));
+                GridTerminalSystem.GetBlocksOfType(_refineries);
+                _refineries.Select(refinery => _settings.RefineryKeywords.Any(keyword => refinery.CustomName.Contains(keyword)));
+                //foreach (var refinery in _refineries)
+                //{
+                //    if (_settings.RefineryKeywords.Any(keyword => refinery.CustomName.Contains(keyword)))
+                //    {
+                //        Echo("Refinery matches keywords");
+                //    }
+                //}
+            }
+            catch (Exception e)
+            {
+                Echo("An error occurred during script execution.");
+                Echo($"Exception: {e}\n---");
+
+                throw;
+            }
         }
     }
 }
---
+++ Scripts/Inventory Management/Pyro's Inventory Manager/Settings.cs
@@ -0,0 +1,47 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        /// <summary>
+        /// Represents user-customizable settings.
+        /// </summary>
+        public class Settings
+        {
+            private readonly MyIni _myIni = new MyIni();
+
+            public List<string> IngotContainerKeywords => _myIni.GetRequiredAsList<string>("Settings", nameof(IngotContainerKeywords));
+
+            public List<string> RefineryKeywords => _myIni.GetRequiredAsList<string>("Settings", nameof(RefineryKeywords));
+
+            public Settings(string configuration)
+            {
+                MyIniParseResult result;
+                var success = _myIni.TryParse(configuration, out result);
+                if (!success)
+                {
+                    throw new ArgumentException($"Could not parse INI configuration while getting settings: {result.Error}");
+                }
+            }
+        }
+    }
+}
